192c7a655582da8405cf21923489ccab1e096bc1
Loup Menjoulet <lmenjoulet@laposte.net>
2023-11-13 23:06:48 +0700

    Config: emacs (polymode)

--- private_dot_emacs.d/init.el
+++ private_dot_emacs.d/init.el
@@ -8,6 +8,9 @@
 (setq inhibit-startup-message t) 
 (setq initial-scratch-message nil)
 
+;; hide warning
+(setq warning-minimum-level :error)
+
 ;; utf-8
 (prefer-coding-system 'utf-8)
 
@@ -93,6 +96,10 @@
 (unless (package-installed-p 'markdown-mode)
   (package-install 'markdown-mode))
 (setq markdown-enable-wiki-links 't)
+(unless (package-installed-p 'polymode)
+  (package-install 'poly-markdown))
+(require 'poly-markdown)
+(add-to-list 'auto-mode-alist '("\\.md" . poly-markdown-mode))
 
 ;; nix config
 (unless (package-installed-p 'nix-mode)
@@ -133,7 +140,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(nix-mode treesit-auto tree-sitter-langs general evil-collection markdown-preview-mode markdown-mode ample-theme evil-smartparens smartparens-config centered-cursor-mode evil)))
+   '(poly-markdown nix-mode treesit-auto tree-sitter-langs general evil-collection markdown-preview-mode markdown-mode ample-theme evil-smartparens smartparens-config centered-cursor-mode evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
