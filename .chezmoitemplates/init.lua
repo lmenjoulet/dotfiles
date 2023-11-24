a929771167df4bfc34916700be59e35f8008d935
Loup Menjoulet <lmenjoulet@laposte.net>
2023-11-24 17:51:49 +0700

    Config: neovim(lsp)

--- .chezmoitemplates/init.lua
+++ .chezmoitemplates/init.lua
@@ -43,6 +43,11 @@ local lsp_settings = {
         }
       }
     }
+  },
+  ltex = {
+    ltex = {
+      completionEnabled = true
+    }
   }
 }
 
@@ -99,13 +104,23 @@ require("lazy").setup {
   {
     "nvim-telescope/telescope.nvim",
     branch = "0.1.x",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope-ui-select.nvim"
+    },
     config = function()
+      require("telescope").setup {
+        extensions = {
+          ["ui-select"] = require("telescope.themes").get_dropdown {}
+        }
+      }
+      require("telescope").load_extension("ui-select")
       local builtin = require("telescope.builtin")
       vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
       vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
       vim.keymap.set('n', '<leader>ft', builtin.live_grep, {}) -- external dependency on ripgrep
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+      vim.keymap.set('n', '<leader>fe', builtin.diagnostics, {})
+      vim.keymap.set('n', "<leader>c", vim.lsp.buf.code_action, {})
     end
   },
   {
